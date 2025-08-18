a3f28c7fd1f3f97a28dd82ada2a985598d2cf2e5
Ridwan Salamony <ridwansalamony@gmail.com>
2025-08-18 17:29:58 +0700

    refactor: error response

--- src/middlewares/error.middleware.ts
+++ src/middlewares/error.middleware.ts
@@ -3,10 +3,20 @@ import { ValidationError } from "yup";
 import { AppError } from "../utils/AppError";
 import { JsonWebTokenError, TokenExpiredError } from "jsonwebtoken";
 import { ApiResponse } from "../utils/ApiResponse";
+import mongoose from "mongoose";
 
 export const errorMiddleware = (err: Error, req: Request, res: Response, next: NextFunction) => {
   if (err instanceof ValidationError) {
     return ApiResponse.error(res, false, 400, err.message, err.errors);
+  }
+
+  if (err instanceof mongoose.Error) {
+    return ApiResponse.error(res, false, 500, err.message, err.name);
+  }
+
+  if ((err as any)?.code) {
+    const _err = err as any;
+    return ApiResponse.error(res, false, 500, _err.errorResponse.errmsg, null);
   }
 
   if (err instanceof AppError) {
