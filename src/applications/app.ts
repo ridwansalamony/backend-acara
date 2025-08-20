eec5ecb622995ff34562a73c0ee7e51eb39f5e35
Ridwan Salamony <ridwansalamony@gmail.com>
2025-08-21 01:24:19 +0700

    refactor: swagger custom css:

--- src/applications/app.ts
+++ src/applications/app.ts
@@ -4,6 +4,11 @@ import cors from "cors";
 import { errorMiddleware } from "../middlewares/error.middleware";
 import swaggerUi from "swagger-ui-express";
 import swaggerOutput from "../docs/swagger-output.json";
+import fs from "fs";
+import path from "path";
+
+// swagger
+const css = fs.readFileSync(path.resolve(__dirname, "../../node_modules/swagger-ui-dist/swagger-ui.css"), "utf-8");
 
 const app = express();
 
@@ -15,7 +20,7 @@ app.use(
   "/api-docs",
   swaggerUi.serve,
   swaggerUi.setup(swaggerOutput, {
-    customCss: "../../node_modules/swagger-ui-dist/swagger-ui.css",
+    customCss: css,
   })
 );
 
